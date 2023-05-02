37ee82cebf74d01c1a41fdf6577ba6ffc7506d82
maheshnehere <maheshnehere93@gmail.com>
2023-05-03 00:12:08 +0530

    connect to database

--- src/com/bridgelabz/Employee_payroll.java
+++ src/com/bridgelabz/Employee_payroll.java
@@ -1,8 +1,25 @@
 package com.bridgelabz;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
 public class Employee_payroll {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to employee payroll program");
+        //Uc1 : Ability to create a payroll service database and have java program connect to database.
+
+        try {
+            //1.Load a driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            //2.Create a Connection
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_service","root","Abhi");
+            if (connection.isClosed()){
+                System.out.println("Connection is closed");
+            }else {
+                System.out.println("Connection is created");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
